e344ce1c256b69b6bd85772363367b6cb417c804
ammonite-dx <123472027+ammonite-dx@users.noreply.github.com>
2023-03-10 21:55:37 +0900

    prepare to use github pages

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/double-cross-dice-calculator/' : './',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
--- vue.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    publicPath: '/double-cross-dice-calculator',
-    outputDir: 'dist',
-}
