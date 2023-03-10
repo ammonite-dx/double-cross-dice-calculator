9fc590f9ba1e60838a40ac41850b558bc8e8f4b6
ammonite-dx <123472027+ammonite-dx@users.noreply.github.com>
2023-03-10 11:51:27 +0900

    change defineConfig-base

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/double-cross-dice-calculator/',
+  base: '/double-cross-dice-calculator',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
