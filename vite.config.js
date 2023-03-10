2841a2a84e9abc4a4aae201625e03e0f730d7bbf
ammonite-dx <123472027+ammonite-dx@users.noreply.github.com>
2023-03-10 22:46:45 +0900

    prepare to use netlify

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/double-cross-dice-calculator/' : './',
   plugins: [
     vue({ 
       template: { transformAssetUrls }
