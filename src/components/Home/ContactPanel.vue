ea09947d3e66d5936479bedb146909126cca1261
ammonite-dx <123472027+ammonite-dx@users.noreply.github.com>
2023-03-27 00:39:41 +0900

    add youtube link

--- src/components/Home/ContactPanel.vue
+++ src/components/Home/ContactPanel.vue
@@ -14,7 +14,7 @@
                 <v-list-item><v-icon :icon="mdiChevronRight"/>DCDCに関する質問・要望がある場合や、不具合を発見した場合は、お手数ですが、紹介動画へコメントするか、作者TwitterのDMまでご連絡ください。</v-list-item>
             </v-list>
             <v-row class="mx-0 my-2">
-                <v-col md="4" cols="12" class="ma-0 pa-2"><v-btn :prepend-icon="mdiYoutube" variant="outlined" block disabled>紹介動画 (Youtube)</v-btn></v-col>
+                <v-col md="4" cols="12" class="ma-0 pa-2"><v-btn href="https://www.youtube.com/watch?v=rwkFp7NDH8w" target="_blank" :prepend-icon="mdiYoutube" variant="outlined" block>紹介動画 (Youtube)</v-btn></v-col>
                 <v-col md="4" cols="12" class="ma-0 pa-2"><v-btn href="https://www.nicovideo.jp/watch/sm41983879" target="_blank" :prepend-icon="niconico" variant="outlined" block>紹介動画 (ニコニコ動画)</v-btn></v-col>
                 <v-col md="4" cols="12" class="ma-0 pa-2"><v-btn href="https://twitter.com/Ammonite_dx" target="_blank" :prepend-icon="mdiTwitter" variant="outlined" block>作者Twitter</v-btn></v-col>
             </v-row>
---
+++ src/components/Home/MoviePanel.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+    import { computed } from 'vue';
+    import { useDisplay } from 'vuetify'
+    import { mdiYoutube } from '@mdi/js';
+    import YouTube from 'vue3-youtube'
+
+    const { width } = useDisplay()
+    const movieWidth = computed(() => Math.min(640,width.value*0.75));
+    const movieHeight = computed(() => movieWidth.value*9.0/16.0)
+
+</script>
+
+<template>
+    <v-card class="ma-0">
+        <v-card-title><v-icon :icon="mdiYoutube"/> 紹介動画</v-card-title>
+        <v-divider class="mx-2" />
+        <v-card-text class="pa-6 ma-0 text-md-body-1 text-caption" style="aspect-ratio: 16/9;">
+            <YouTube src="https://www.youtube.com/watch?v=rwkFp7NDH8w" @ready="onReady" ref="youtube" width="100%"/>
+        </v-card-text>
+    </v-card>
+</template>
