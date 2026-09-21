5e37ca3ecdc13442859ef665ada63f41ad671a23
Ammonite <ammonitedx@gmail.com>
2026-09-21 23:42:56 +0900

    test: repair damage precision audit entrypoint

--- experiments/r23-damage-summary-precision/runtime-entry.mjs
+++ experiments/r23-damage-summary-precision/runtime-entry.mjs
@@ -1,8 +1,6 @@
 export { calculateD10Distribution } from '../../src/calculation/D10Calculator.js'
-export {
-  calculateDamageOnDemand,
-  getDamageStatistics,
-} from '../../src/calculation/DamageCalculator.js'
+export { calculateDamageOnDemand } from '../../src/calculation/DamageCalculator.js'
+export { getDamageStatistics } from '../../src/calculation/DamageStatistics.js'
 export { generateMixedDamageDistribution } from '../../src/calculation/RuntimeDamageRollCalculator.js'
 export { calculateDxDistribution } from '../../src/calculation/DxCalculator.js'
 export { createCalculationClient } from '../../src/runtime/CalculationClient.ts'
